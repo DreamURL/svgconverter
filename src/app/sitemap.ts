44be144e39babcf6069320ad51fd4ee5e08f59bf
DreamURL <oodework@gmail.com>
2026-03-04 19:51:47 +0900

    sitemap update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,25 +5,25 @@ export const revalidate = false
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://dreamurl.github.io/svgconverter',
+      url: 'https://svgconverter.dreamurl.biz',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://dreamurl.github.io/svgconverter/free-svg-converter',
+      url: 'https://svgconverter.dreamurl.biz/free-svg-converter',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: 'https://dreamurl.github.io/svgconverter/svg-editor-online',
+      url: 'https://svgconverter.dreamurl.biz/svg-editor-online',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: 'https://dreamurl.github.io/svgconverter/react-svg-components',
+      url: 'https://svgconverter.dreamurl.biz/react-svg-components',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
